9c4ce1249acf1f2e194eb340c6951014f36b69b1
marinaongithub <73482347+marinaongithub@users.noreply.github.com>
2024-06-06 00:38:31 +0100

    Added hamburger and toggle menu function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 import logo from '../../assets/images/logo.svg'
 import './Header.css'
 
+
 const Header = () => {
+
+  const [isNavVisible, setIsNavVisible] = useState(false)
+
+  const toggleMenu = () => {
+    setIsNavVisible(!isNavVisible)
+  }
+
+  
   return (
     <header>
         <nav className='flex'>
-            <img src={logo} alt="sunnyside agency logo" />
-            <ul className='barlow-semibold flex justify-center text-center'>
+            <img src={logo} className="logo" alt="sunnyside agency logo" width="124" height="24" />
+            <button className="mobile-nav-toggle" onClick={toggleMenu} aria-controls="nav-list">
+                <span className="sr-only" aria-expanded={isNavVisible}>
+                </span>
+            </button>  
+            <ul className={`barlow-semibold flex justify-center text-center ${isNavVisible ? 'visible' : ''}`}>
                 <li>About</li>
                 <li>Services</li>
                 <li>Project</li>
