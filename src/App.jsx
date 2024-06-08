645542f63e0d04f45bfb0a9ce400c7fc597b4ce1
marinaongithub <73482347+marinaongithub@users.noreply.github.com>
2024-06-08 16:37:04 +0100

    Added and styled txtCard

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,26 @@ import glass from '../src/assets/images/mobile/image-stand-out.jpg'
 
 function App() {
 
+  const cards = {
+    card1: {
+      "h2": "Transform your brand",
+      "p": "We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.",
+      "color": "yellow"
+    },
+    card2: {
+      "h2": "Stand out to the right audience",
+      "p": "Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. ",
+      "color": "red",
+    }
+  }
+
   return (
     <>
       <Header></Header>
       <ImgCard src={egg} alt="egg image with yellow background"></ImgCard>
-      <TxtCard></TxtCard>
+      <TxtCard h2={cards.card1.h2} p={cards.card1.p} color={cards.card1.color}></TxtCard>
       <ImgCard src={glass} alt="glass image with orange background"></ImgCard>
+      <TxtCard h2={cards.card2.h2} p={cards.card2.p} color={cards.card2.color}></TxtCard>
       <TxtImgCard></TxtImgCard>
       <ImgGallery></ImgGallery>
       <TxtImgCard></TxtImgCard>
--- src/components/TxtCard/TxtCard.jsx
+++ src/components/TxtCard/TxtCard.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './TxtCard.css'
 
-const TxtCard = () => {
+const TxtCard = (props) => {
   return (
-    <div>TxtCard</div>
+    <section className='txtCard text-center'>
+      <h2 className='fraunces-black'>{props.h2}</h2>
+      <p className='barlow-semibold'>{props.p}</p>
+      <button className={`fraunces-black learnMore ${props.color}`}>Learn more</button>
+    </section>
   )
 }
 
